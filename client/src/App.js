546be63e491479a2866f5706f4b43ebc98d6f9c5
denist2322 <parksomang125@gmail.com>
2022-10-05 13:10:32 +0900

    axios 추가, 프록시 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [test, setTest] = useState("");
+  useEffect(() => {
+    const doTest = async () => {
+      const testData = await axios({
+        url: `http://localhost:5000`,
+        method: "GET",
+      });
+      setTest(testData.data);
+    };
+    doTest();
+  });
+
+  return <div className="App">{test}</div>;
 }
 
 export default App;
